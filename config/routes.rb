8373d3f932134e3f23a7de020163a8a05a245473
EC2 Default User <ec2-user@ip-172-31-1-175.ap-northeast-1.compute.internal>
2023-02-02 13:01:31 +0000

    [fix]part-temparate

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  root to: "homes#top"
+  get 'homes/about'
+  devise_for :users
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:new, :index, :show, :create, :edit, :destroy]
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
